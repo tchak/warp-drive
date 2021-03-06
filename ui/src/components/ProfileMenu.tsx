0140c42da1406f6aa1a3c8fa9f01ed050c48fccb
Paul Chavard <github@paul.chavard.net>
2021-03-06 19:37:11 +0100

    Improuve profile menu

--- ui/src/components/ProfileMenu.tsx
+++ ui/src/components/ProfileMenu.tsx
@@ -1,69 +1,65 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { HiOutlineChevronDown } from 'react-icons/hi';
 import Avatar from 'react-avatar';
+import { Transition, Menu } from '@headlessui/react';
 import { Link } from 'react-router-dom';
 
-export function ProfileMenu({ name, email }: { name?: string; email: string }) {
-  const [menuOpen, setMenuOpen] = useState(false);
-
+export function ProfileMenu({ email, name }: { email: string; name?: string }) {
   return (
-    <>
-      <div>
-        <button
-          type="button"
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="max-w-xs bg-white rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 lg:p-2 lg:rounded-md lg:hover:bg-gray-50"
-          id="user-menu"
-          aria-haspopup="true"
-        >
-          <Avatar name={name ?? email} size="30" round={true} />
-          <span className="hidden ml-3 text-gray-700 text-sm font-medium lg:block">
-            <span className="sr-only">Open user menu for </span>
-            {name ?? email}
-          </span>
-          <HiOutlineChevronDown className="hidden flex-shrink-0 ml-1 h-5 w-5 text-gray-400 lg:block" />
-        </button>
-      </div>
-      {/* <!--
-Profile dropdown panel, show/hide based on dropdown state.
+    <Menu>
+      {({ open }) => (
+        <div className="ml-3 relative">
+          <div>
+            <Menu.Button
+              className="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:bg-cool-gray-100 lg:p-2 lg:rounded-md lg:hover:bg-cool-gray-100"
+              aria-label="User menu"
+              aria-haspopup="true"
+            >
+              <Avatar email={email} round={true} size="2em" />
+              <p className="hidden ml-3 text-cool-gray-700 text-sm leading-5 font-medium lg:block">
+                {name ?? email}
+              </p>
+              <HiOutlineChevronDown className="hidden flex-shrink-0 ml-1 text-cool-gray-400 lg:block" />
+            </Menu.Button>
+          </div>
 
-Entering: "transition ease-out duration-100"
-From: "transform opacity-0 scale-95"
-To: "transform opacity-100 scale-100"
-Leaving: "transition ease-in duration-75"
-From: "transform opacity-100 scale-100"
-To: "transform opacity-0 scale-95"
---> */}
-      {menuOpen && (
-        <div
-          className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5"
-          role="menu"
-          aria-orientation="vertical"
-          aria-labelledby="user-menu"
-        >
-          <Link
-            to="/account"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem"
+          <Transition
+            show={open}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
-            Your Profile
-          </Link>
-          <Link
-            to="/settings"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem"
-          >
-            Settings
-          </Link>
-          <Link
-            to="/logout"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-            role="menuitem"
-          >
-            Logout
-          </Link>
+            <Menu.Items
+              static
+              className="absolute right-0 w-48 mt-2 origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+              aria-orientation="vertical"
+              aria-labelledby="user-menu"
+            >
+              <Menu.Item>
+                <Link
+                  to="/profile"
+                  className="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
+                  role="menuitem"
+                >
+                  Your Profile
+                </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link
+                  to="/signout"
+                  className="block px-4 py-2 text-sm text-cool-gray-700 hover:bg-cool-gray-100 transition ease-in-out duration-150"
+                  role="menuitem"
+                >
+                  Logout
+                </Link>
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
         </div>
       )}
-    </>
+    </Menu>
   );
 }
